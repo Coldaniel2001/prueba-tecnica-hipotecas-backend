1aac994a868525be67ae64295de7a5e21c543d76
Daniel Wilson <danielvillenawilson@gmail.com>
2023-10-18 16:38:28 +0200

    change(get client):add function get one client with DNI

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -4,13 +4,39 @@ const createClient = async (req, res) => {
     const { name, dni, gmail } = req.body
 
     try {
+        const dniExist = await UserModel.findOne({ dni })
+        const gmailExist = await UserModel.findOne({ email: gmail })
+        if (dniExist && !gmailExist) {
+            console.log(dni)
+            return res.status(200).send({ status: 'EXISTE DNI' })
+        }else if(gmailExist && !dniExist){
+            console.log(dni)
+            return res.status(200).send({ status: 'EXISTE Gmail' })
+        }else if(dniExist && gmailExist){
+            console.log(dni)
+            return res.status(200).send({ status: 'EXISTE Gmail y DNI' })
+        }else{
+            console.log(dni)
+            const newClient = await UserModel.create({
+                name,
+                dni,
+                email: gmail,
+            })
+            return res.status(200).send({ status: 'OK', newClient })
+        }
 
-        const newClient = await UserModel.create({
-            name,
-            dni,
-            email: gmail,
-        })
-        return res.status(200).send({ status: 'OK', newClient })
+    } catch (error) {
+        res.status(500).send({ status: 'FALSE' })
+    }
+}
+const getOneClient = async (req, res) => {
+    const { clientDni } = req.params
+
+    try {
+
+        const getOneClient = await UserModel.findOne({ dni: clientDni })
+
+        return res.status(200).send({ status: 'OK', getOneClient })
 
     } catch (error) {
         res.status(500).send({ status: 'FALSE' })
@@ -19,4 +45,4 @@ const createClient = async (req, res) => {
 
 
 
-module.exports = { createClient }
+module.exports = { createClient, getOneClient }
